49383a70073661fd4a2f0e73ff9822ff45500d1f
maisiembleton <maisie.embleton@gmail.com>
2019-06-02 00:29:43 +1200

    mobile optimised landing kind of

--- index.js
+++ index.js
@@ -63,7 +63,10 @@ function setup() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(
+    document.documentElement.clientWidth,
+    document.documentElement.clientHeight
+  );
 }
 
 function draw() {
@@ -140,36 +143,11 @@ function mouseMoved() {
 function mousePressed() {
   useFill = !useFill;
 }
-//FUNCTIONS FOR THE SLIDESHOW ref:https://www.w3schools.com/howto/howto_js_slideshow.asp
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+function touchMoved() {
+  allParticles.push(new Particle(mouseX, mouseY, maxLevel));
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("image-slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+function touchStarted() {
+  useFill = !useFill;
 }
